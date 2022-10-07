ca6121f422d00e3954e04db2a9a57c833a054224
BangBungKop <115243943+BangBungKop@users.noreply.github.com>
2022-10-07 08:04:12 -0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -2,7 +2,7 @@
 
 clear
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m          ⇱ SC VPS BY RIDWAN ⇲          \E[0m"
+echo -e "\E[44;1;39m          ⇱ SC VPS BY NU ⇲          \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
@@ -16,5 +16,5 @@ echo -e "# My Family                                     #"
 echo -e "# LamVpn                                        #"
 echo -e "# Horasss                                       #"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m          ⇱ RIDWAN-STORE ⇲          \E[0m"
+echo -e "\E[44;1;39m          ⇱ NU-STORE ⇲          \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
